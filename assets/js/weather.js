e44fb664f67ac4fff93e3dc85455342903016eb8
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-06-30 09:23:32 -0400

    fetch api by city name

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -11,7 +11,7 @@ var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    var city = cityInputEl.nodeValue.trim();
+    var city = cityInputEl.value.trim();
     if(city) {
         getCityWeather(city);
         get5Day(city);
@@ -23,3 +23,20 @@ var formSubmitHandler = function(event) {
     saveSearch();
     pastSearch(city);
 }
+
+var saveSearch = function() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+
+var getCityWeather = function() {
+    var apikey = "034e0d3feab0beaed53510b99ffd11c3"
+    var apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid={API key}`
+
+    fetch(apiURL)
+    .then(function(respose){
+        response.json().then(function(data){
+            displayWeather(data, city);
+        });
+    });
+};

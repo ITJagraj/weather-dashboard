2e284e2789160eda23eb312d328f894ab0b3b755
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-07-13 01:22:42 -0400

    image element

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -9,13 +9,13 @@ var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     var city = cityInputEl.value.trim();
-    if(city) {
+    if (city) {
         getCityWeather(city);
         get5Day(city);
-        cities.unshift({city});
+        cities.unshift({ city });
         cityInputEl.value = "";
     } else {
         alert("Please enter a city");
@@ -24,19 +24,36 @@ var formSubmitHandler = function(event) {
     pastSearch(city);
 }
 
-var saveSearch = function() {
+var saveSearch = function () {
     localStorage.setItem("cities", JSON.stringify(cities));
 };
 
 
-var getCityWeather = function() {
+var getCityWeather = function () {
     var apikey = "034e0d3feab0beaed53510b99ffd11c3"
     var apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid={API key}`
 
     fetch(apiURL)
-    .then(function(respose){
-        response.json().then(function(data){
-            displayWeather(data, city);
+        .then(function (respose) {
+            response.json().then(function (data) {
+                displayWeather(data, city);
+            });
         });
-    });
 };
+
+var displayWeather = function (weather, searchCity) {
+    //clear old content
+    weatherContainerEl.textContent = "";
+    citySearchInputEl.textContent = searchCity;
+}
+
+
+//create date element
+var currentDate = document.createElement("span")
+currentDate.textContent = "(" + SVGAnimateMotionElement(weather.dt.value).format("MMMM D, YYYY") + ") ";
+citySearchInputEl.appendChild(currentDate);
+
+//create an image element
+   var weatherIcon = document.createElement("img")
+   weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+   citySearchInputEl.appendChild(weatherIcon);
